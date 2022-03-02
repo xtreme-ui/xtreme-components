5d651f5e5d026f6f658cb9a1b01050ed06e3dab4
Will Parks <williamparks94@icloud.com>
2022-03-02 10:35:22 -0500

    Create React Component Fixes #1

---
+++ src/components/sick-button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SickButton = () => {
+    return (
+        <div>
+            <button onClick={alert("Incredible, wow!")}>Hello Button</button>
+        </div>
+    );
+};
